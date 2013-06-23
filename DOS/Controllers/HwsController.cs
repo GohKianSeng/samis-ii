65eae4eff48fe2a4be927ae30d52f6ec66807bcb
ks <ks@ks>
2013-06-24 00:34:27 +0800

    Fix Postal Code Issue

---
+++ DOS/Controllers/HwsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOS.Controllers
+{
+    public class HwsController : MasterPageController
+    {
+        //
+        // GET: /Hws/
+
+        public ActionResult AddNewMember()
+        {
+            return View();
+        }
+
+    }
+}
